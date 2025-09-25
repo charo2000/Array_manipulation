019f4d8570900e6cdd76761f2b015f25392add4f
WILSON-KAMGNE <wilsonkamgne@icloud.com>
2025-09-25 11:49:44 +0100

    Create SommeMatrice.h

---
+++ code/SommeMatrice.h
@@ -0,0 +1,10 @@
+#ifndef SOMMEMATRICES_H
+#define SOMMEMATRICES_H
+
+// Lecture sécurisée d'un entier
+int lireEntier(const char *message);
+
+// Affiche une matrice de dimensions m x n
+void afficherMatrice(int m, int n, int matrice[m][n]);
+
+#endif

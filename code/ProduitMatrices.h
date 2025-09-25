d0a8c4391052d2c3a83aceab6775dace77edefc7
WILSON-KAMGNE <wilsonkamgne@icloud.com>
2025-09-25 11:19:02 +0100

    Create ProduitMatrices.h

---
+++ code/ProduitMatrices.h
@@ -0,0 +1,10 @@
+#ifndef MATRICES_H
+#define MATRICES_H
+
+// Lecture sécurisée d'un entier
+int lireEntier(const char *message);
+
+// Affiche une matrice de dimensions lignes x colonnes
+void afficherMatrice(int lignes, int colonnes, int matrice[lignes][colonnes]);
+
+#endif
